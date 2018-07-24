b9c91baa80957166cf3674c452b7f6d7514424fc
Leonardo Batista <leonardohenriquebatista@gmail.com>
2018-07-24 13:15:25 -0300

    Removed generated content and added routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Person from './pages/Person';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route path='/' component={Home} />
+          <Route path='/person/:id' component={Person} />
+        </Switch>
       </div>
     );
   }
