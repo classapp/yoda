c2ddda5cbd2e9a0cd82be10faf3e4e6d7a9b5911
Leonardo Batista <leonardohenriquebatista@gmail.com>
2018-07-26 10:51:39 -0300

    Person request

--- src/pages/Person.js
+++ src/pages/Person.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import axios from 'axios';
+import { Container, Header, Button, Icon, Message } from 'semantic-ui-react';
 
 export default class Person extends React.Component {
+
+  componentWillMount() {
+    const { id } = this.props.match.params;
+    const url = `https://swapi.co/api/people/${id}`;
+    axios.get(url)
+      .then((res) => {
+        this.setState({
+          person: res.data,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   render() {
     return(<p>Person</p>);
   }
