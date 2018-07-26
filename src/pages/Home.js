88243346e55f8bc68b72dc71a778b91820da160a
Leonardo Batista <leonardohenriquebatista@gmail.com>
2018-07-26 14:25:46 -0300

    Added key to each item segment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -81,7 +81,6 @@ export default class Home extends React.Component {
 
     const resultItems = !!results && results.map((r) => {
       let id = _.nth(r.url.split("/"),-2);
-      return <Segment vertical><Link to={`/person/${id}`}>{r.name}</Link></Segment>;
     });
 
     const resultsSection = (
